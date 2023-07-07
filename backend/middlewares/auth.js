4faad4c495e49bea6fb8a3554296a38ade0185ee
Maxim Perepletchikov <maxwell-38@yandex.ru>
2023-07-07 15:45:13 +0500

    не работает

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -2,18 +2,37 @@ const jwt = require('jsonwebtoken');
 const AuthorizationError = require('../errors/AuthorizationError');
 
 const auth = (req, res, next) => {
-  const token = req.cookies.jwt;
+  // const token = req.cookies.jwt;
+
+  // let payload;
+
+  // try {
+  //   payload = jwt.verify(token, 'SECRET');
+  // } catch (error) {
+  //   throw new AuthorizationError('Необходима авторизация');
+  // }
+
+  // req.user = payload;
+
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    next(new AuthorizationError('Необходима авторизация'));
+    return;
+  }
+  const token = authorization.replace('Bearer ', '');
   let payload;
 
   try {
     payload = jwt.verify(token, 'SECRET');
-  } catch (error) {
-    throw new AuthorizationError('Необходима авторизация');
+  } catch (err) {
+    next(new AuthorizationError('Необходима авторизация'));
+    return;
   }
 
   req.user = payload;
 
-  return next();
+  next();
 };
 
 module.exports = auth;
