55e24769dcd74902ab5a5e52015e8590eb498fb7
Maxim Perepletchikov <maxwell-38@yandex.ru>
2023-07-27 08:39:57 +0500

    fixed: #1

--- backend/app.js
+++ backend/app.js
@@ -6,8 +6,8 @@ const helmet = require('helmet');
 const limiter = require('./middlewares/limiter');
 const cors = require('./middlewares/cors');
 const routes = require('./routes');
-const { errorHandler } = require('./middlewares/error');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const { errorHandler } = require('./middlewares/error');
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
